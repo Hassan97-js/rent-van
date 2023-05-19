6a4b0b2e36bf2a37b3c3bb74eb3a1d2c6d6007a3
Hassan <hassan.sweden.dev@gmail.com>
2023-05-19 14:31:30 +0200

    Commit
    refactoring 2

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,8 @@ import Footer from "./Footer";
 
 import Button from "./generic/Button";
 import Tag from "./generic/Tag";
-import Spinner from "./generic/Spinner";
 import ErrorElement from "./generic/ErrorElement";
 
 import VanList from "./van-list/VanList";
 
-export { Button, VanList, Tag, Spinner, ErrorElement, Header, Footer };
+export { Button, VanList, Tag, ErrorElement, Header, Footer };
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { Spinner } from "./components";
-
 import { routerConfig } from "./data";
 
 import "./data/server";
 
-import "./scss/styles.scss";
+import "./styles/reset.css";
+import "./styles/main.css";
+import "./styles/utils.css";
 
 const router = createBrowserRouter(routerConfig);
 
@@ -16,6 +16,6 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <RouterProvider router={router} fallbackElement={<Spinner />} />
+    <RouterProvider router={router} fallbackElement={<h1>Loading...</h1>} />
   </StrictMode>
 );
